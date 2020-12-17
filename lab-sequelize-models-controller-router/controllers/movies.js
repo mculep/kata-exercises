6f23c2d3cf52fcfa2765299f0876e22e2941cbcc
mculep <mculep@gmail.com>
2020-12-16 21:16:13 -0800

    added movies

---
+++ lab-sequelize-models-controller-router/controllers/movies.js
@@ -0,0 +1,44 @@
+const { INTEGER } = require('sequelize');
+const { Movie } = require('../models');
+const { layout } = require('../utils');
+
+const list = async (req, res) => {
+    const movies = await Movie.findAll();
+    res.render('movies/list', {
+        locals: {
+            movies
+        },
+        ...layout
+    });
+};
+
+const showForm = (req, res) => {
+    res.render('movies/form', {
+        ...layout
+    });
+};
+
+const processForm = async (req, res) => {
+    const {
+        title,
+        genre,
+        year,
+        rating
+    } = req.body;
+    const newMovie = await Movie.create({
+        title,
+        genre,
+        year: parseInt(year, 10),
+        rating: parseInt(rating, 10)
+    });
+    console.log(newMovie);
+    res.redirect(req.baseUrl);
+};
+
+module.exports = {
+    list,
+    showForm,
+    processForm
+};
+
+
---
+++ lab-sequelize-models-controller-router/routers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    homeRouter: require('./home'),
+    snackRouter: require('./snacks'),
+    movieRouter: require('./movies'),
+    
+}
